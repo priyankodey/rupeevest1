610189ed6ce2e07ee843958b5d0a436ea6baf555
Varun <varunmundra.iitkgp@gmail.com>
2013-12-30 15:39:49 -0800

    tax saving page2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,14 +31,5 @@ Beta1::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-    config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: 'mail.google.com',
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: 'varun11us@gmail.com',
-    password: '12333333'
-    }
 
 end
